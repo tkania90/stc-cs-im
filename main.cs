ce81d9bc55e89a2f06739fbc5e22d409b9122795
Tomasz Kania <tomasz-kania@outlook.com>
2026-08-01 06:15:52 +0200

    done

--- main.cs
+++ main.cs
@@ -1,10 +1,27 @@
 using System;
 
-// class Pair<A, B> { ... }
+class Pair<A, B>
+{
+    A a;
+    B b;
 
-class Program {
-    static void Main() {
-        // var p = new Pair<string, int>("Ada", 36);
-        // Console.WriteLine(p);
+    public Pair(A a, B b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    public string ToString()
+    {
+        return $"({a}, {b})";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var p = new Pair<string, int>("Ada", 36);
+        Console.WriteLine(p.ToString());
     }
 }
