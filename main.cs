4374c03fc46f05b8dad363c74a674a600a878f46
Tomasz Kania <tomasz-kania@outlook.com>
2026-08-01 05:50:45 +0200

    start C# Intermediate

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+// class Pair<A, B> { ... }
+
+class Program {
+    static void Main() {
+        // var p = new Pair<string, int>("Ada", 36);
+        // Console.WriteLine(p);
+    }
+}
